b0536e853aa692b6a6607121592396f78239597e
TheWalrus204 <106426946+TheWalrus204@users.noreply.github.com>
2022-06-27 13:47:25 -0700

    Update magTest.py
    completed test script

--- magTest.py
+++ magTest.py
@@ -9,18 +9,54 @@ print('Start')
 
 kit = MotorKit()
 
+
+step = 1/10
+ramp = step/1
+xRange = 1/step
+
 # 0V for 10 secs
+kit.motor1.throttle = None
+sleep(10)
+
 # ramp 5V for 1 sec
+for i in range(xRange):
+    kit.motor1.throttle = (ramp*i)
+    sleep(step)
+
 # 5V for 10 sec
+kit.motor1.throttle = 1
+sleep(10)
+
 # ramp 0V for 1 sec
+for i in range(xRange):
+    kit.motor1.throttle = 1 - (ramp*i)
+    sleep(step)
+
 # 0V for 10 secs
+kit.motor1.throttle = 0
+sleep(10)
+
 # ramp to -5V for 1 sec
+for i in range(xRange):
+    kit.motor1.throttle = 0 - (ramp*i)
+    sleep(step)
+
 # -5V for 10 sec
+kit.motor1.throttle = -1
+sleep(10)
+
 # ramp 0V for 1 sec
+for i in range(xRange):
+    kit.motor1.throttle = -1 + (ramp*i)
+    sleep(step)
+
 # 0V for 10 secs
+kit.motor1.throttle = 0
+sleep(10)
+
 # End
 
-for i in range(50):
+'''for i in range(50):
     kit.motor1.throttle = 1.0 - 2*(i/50)
     sleep(0.1)
         
@@ -43,5 +79,6 @@ sleep(1)
 kit.motor3.throttle = None
 sleep(1)
 kit.motor3.throttle = 0.0
+'''
 
 print('Done')
