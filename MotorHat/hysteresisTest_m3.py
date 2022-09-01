4726c57a1f55ff42f0f279382db22a85d108b94e
mikettt3 <95988405+mikettt3@users.noreply.github.com>
2022-08-31 20:34:36 -0700

    Add files via upload
    Hysteresis testing MTQ3 day 2

--- MotorHat/hysteresisTest_m3.py
+++ MotorHat/hysteresisTest_m3.py
@@ -10,38 +10,50 @@ print('Start')
 kit = MotorKit()
 
 Throttle = 1.0
-freq = 5   # hertz
+freq = 10   # hertz
 period = 1/freq
-step = period/4
+step = period/2
 
+kit.motor1.throttle = 0
+kit.motor2.throttle = 0
 kit.motor3.throttle = 0
 print('Throttle = 0')
 sleep(1)
 
+# kit.motor1.throttle = Throttle
+# kit.motor2.throttle = Throttle
 # kit.motor3.throttle = Throttle
 
+print('On')
 while True:
-    for i in range(10):
-        kit.motor3.throttle = (0.1*i*Throttle)
-        sleep(step)
+    kit.motor3.throttle = Throttle
+    sleep(step)
+    kit.motor3.throttle = -Throttle
+    sleep(step)
 
-    print('Done 1')
-    
-    for i in range(20):
-        kit.motor3.throttle = Throttle - (0.1*i*Throttle)
-        sleep(step)
 
-    print('Done 2')
-    
-    for i in range(10):
-        kit.motor3.throttle = 0 - (0.1*i*Throttle)
-        sleep(step)
-
-    print('Done 3')
-    
-    for i in range(10):
-        kit.motor3.throttle = -Throttle + (0.1*i*Throttle)
-        sleep(step)
+# while True:
+#     for i in range(10):
+#         kit.motor3.throttle = (0.1*i*Throttle)
+#         sleep(step)
+# 
+#     print('Done 1')
+#     
+#     for i in range(20):
+#         kit.motor3.throttle = Throttle - (0.1*i*Throttle)
+#         sleep(step)
+# 
+#     print('Done 2')
+#     
+#     for i in range(10):
+#         kit.motor3.throttle = 0 - (0.1*i*Throttle)
+#         sleep(step)
+# 
+#     print('Done 3')
+#     
+#     for i in range(10):
+#         kit.motor3.throttle = -Throttle + (0.1*i*Throttle)
+#         sleep(step)
         
 print('Done all')
 
